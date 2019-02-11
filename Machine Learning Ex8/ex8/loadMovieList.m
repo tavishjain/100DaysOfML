75669ba04f4f01f74e7c99e2823ba858cd009ac1
tavishjain <jaintavish@gmail.com>
2019-02-11 23:36:28 +0530

    Anomaly Detection n and Recommender System

---
+++ Machine Learning Ex8/ex8/loadMovieList.m
@@ -0,0 +1,17 @@
+function movieList = loadMovieList()
+%GETMOVIELIST reads the fixed movie list in movie.txt and returns a
+%cell array of the words
+%   movieList = GETMOVIELIST() reads the fixed movie list in movie.txt 
+%   and returns a cell array of the words in movieList.
+
+
+%% Read the fixed movieulary list
+fid = fopen('movie_ids.txt');
+
+muT = (1 / m) * sum(X);
+mu = muT';
+sigma2T = (1 / m) * sum((X - muT).^2);
+sigma2 = sigma2T';
+
+
+end
